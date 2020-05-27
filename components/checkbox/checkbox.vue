5ba1c6eb3e27f11b4fa48bd24d7b9ba3669eebc2
zhangjingguo_19080088 <zhangjingguo@shinho.net.cn>
2020-05-27 17:20:39 +0800

    fix: :update: checkbox

--- components/checkbox/checkbox.vue
+++ components/checkbox/checkbox.vue
@@ -1,14 +1,28 @@
+<!--
+ * @Descripttion: checkbox组件
+ * @Author: asyncnode
+ * @Date: 2020-05-27 08:45:41
+ * @LastEditors: asyncnode
+ * @LastEditTime: 2020-05-27 16:07:06
+-->
 <template>
-  <label class="hp-checkbox"></label>
+  <label :class="wrapperClasses">
+    span.hp-checkbox_
+  </label>
 </template>
 
 <script>
+const prefix = 'hp-checkbox';
 export default {
-  components: {},
+  name: 'HpCheckbox',
   data() {
     return {};
   },
-  computed: {},
+  computed: {
+    wrapperClasses() {
+      return [`${prefix}`];
+    }
+  },
   mounted() {},
   methods: {}
 };
